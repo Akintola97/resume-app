177915be15abe21212599e3f6d38546b50ffd8c6
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-10-02 14:58:47 -0500

    update kinde auth

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,3 +1,16 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = handleAuth();
+// export const GET = handleAuth();
+
+// app/api/auth/[kindeAuth]/route.js
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function GET(req, context) {
+  const authHandler = handleAuth();
+  return authHandler(req, context);
+}
+
+export async function POST(req, context) {
+  const authHandler = handleAuth();
+  return authHandler(req, context);
+}
